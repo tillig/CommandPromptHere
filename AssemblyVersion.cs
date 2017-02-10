f1ee7e7ebfa345fc57a50da421a552cf572b4f64
Travis Illig <travis.illig@silverlining.ventures>
2017-02-10 11:33:36 -0800

    Updated version.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -4,8 +4,7 @@ using System.Resources;
 [assembly: AssemblyCompany("Travis Illig")]
 [assembly: AssemblyProduct("Command Prompt Here Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
